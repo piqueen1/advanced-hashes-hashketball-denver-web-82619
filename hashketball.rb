b0d4099abd3a4a025f9dc1f54b07cfa7d025f65b
Finn Thye <piqueen@gmail.com>
2019-08-12 21:30:08 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,9 +1,39 @@
 def game_hash
   teams = {}
   teams[:home] = {
-    :team_name => "",
-    :colors => [],
-    :players => [{
+    :team_name => "Brooklyn Nets",
+    :colors => ["black", "white"],
+    :players => [ "Alan Anderson" => {
+      :number => "0",
+      :shoe => "16",
+      :points => 22,
+      :rebounds => 12,
+      :assists => 12,
+      :steals => 3,
+      :blocks => 1,
+      :slam_dunks => 1
+      },
+      "Reggie Evans" => {
+      :number => "30",
+      :shoe => "14",
+      :points => 12,
+      :rebounds => 12,
+      :assists => 12,
+      :steals => 12,
+      :blocks => 12,
+      :slam_dunks => 7
+      },
+      "Brook Lopez" => {
+      :number => "11",
+      :shoe => "17",
+      :points => 17,
+      :rebounds => 19,
+      :assists => 10,
+      :steals => 3,
+      :blocks => 1,
+      :slam_dunks => 15
+      },
+      "Mason Plumlee" => {
       :number => "",
       :shoe => "",
       :points => 0,
@@ -12,6 +42,16 @@ def game_hash
       :steals => 0,
       :blocks => 0,
       :slam_dunks => 0
+      },
+      "Jason Terry" => {
+      :number => "31",
+      :shoe => "15",
+      :points => 19,
+      :rebounds => 2,
+      :assists => 2,
+      :steals => 4,
+      :blocks => 11,
+      :slam_dunks => 1
       }]
     }
   teams[:away] = {
@@ -35,3 +75,4 @@ end
 def num_points_scored
 
 end
+
