9640efdea1e1d338e13efd14cd76179dc648554b
Finn Thye <piqueen@gmail.com>
2019-08-12 21:02:30 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,18 +1,37 @@
 def game_hash
-  hash = {}
-  hash[home] = {
+  teams = {}
+  teams[:home] = {
     :team_name => "",
     :colors => [],
     :players => [{
-      :number => ,
-      :shoe => ,
-      :points => ,
-      :rebounds => ,
-      :assists => ,
-      :steals => ,
-      :blocks => ,
-      :slam_dunks =>
-    }]
-  }
-  hash[away] = {}
-  
+      :number => "",
+      :shoe => "",
+      :points => 0,
+      :rebounds => 0,
+      :assists => 0,
+      :steals => 0,
+      :blocks => 0,
+      :slam_dunks => 0
+      }]
+    }
+  teams[:away] = {
+    :team_name => "",
+    :colors => [],
+    :players => [{
+      :number => "",
+      :shoe => "",
+      :points => 0,
+      :rebounds => 0,
+      :assists => 0,
+      :steals => 0,
+      :blocks => 0,
+      :slam_dunks => 0
+      }]
+    }
+    
+  teams
+end
+
+def num_points_scored
+
+end
