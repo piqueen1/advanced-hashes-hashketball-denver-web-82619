7611ddf1c4e572f49ab1fcf59dca50a96e1aa558
Finn Thye <piqueen@gmail.com>
2019-08-13 23:27:20 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -219,23 +219,11 @@ def big_shoe_rebounds
   matching_rebounds
 end
 
-
-=begin
-def good_practices
-  game_hash.each do |location, team_data|
-    #are you ABSOLUTELY SURE what 'location' and 'team data' are? use binding.pry to find out!
-    #binding.pry
-    team_data.each do |attribute, data|
-      #are you ABSOLUTELY SURE what 'attribute' and 'team data' are? use binding.pry to find out!
-      #binding.pry
- 
-      #what is 'data' at each loop throughout .each block? when will the following line of code work and when will it break?
-      data.each do |data_item|
-          binding.pry
-      end
-    end
-  end
+def most_points_scored
 end
-=end
 
-#good_practices
+def winning_team
+end
+
+def player_with_longest_name
+end
